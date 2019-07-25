5f7e45fc9f3d30e98d52ff3e1888843bff0632e0
toniahurst <antonia.a.hurst@gmail.com>
2019-07-25 02:21:36 +0000

    created title model and adjusted _create_titles.rb

---
+++ db/migrate/20190725021949_create_titles.rb
@@ -0,0 +1,12 @@
+class CreateTitles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :titles do |t|
+    	t.string :name
+    	t.string :author
+    	t.string :description
+
+
+      t.timestamps
+    end
+  end
+end
